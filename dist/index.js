ad6cbf8444d9030752b21c5d9c07b0ecfcfe8021
Dave Schmitz <dgschmitz@symplr.com>
2023-12-29 12:16:37 -0500

    chore: update comment formatting

--- dist/index.js
+++ dist/index.js
@@ -28935,11 +28935,11 @@ async function run() {
     output += `\n#### Terraform Initialization ⚙️ \`steps.init.outcome\``
     output += `\n#### Terraform Validation 🤖 \`steps.validate.outcome\``
     output += `\n<details><summary>Validation Output</summary> \
-    \
+    \n\
     \n\`\`\`\n \
-    Success! The configuration is valid. \
-    \`\`\` \
-    \
+    \nSuccess! The configuration is valid. \
+    \n\`\`\` \
+    \n\
     </details>`
 
     output += `\n\n#### Terraform Plan 📖 \`steps.plan.outcome\``
